f21f7303f2e3562d24a5190069b0bfc21e717716
Nelson An <hoangquangan63@gmail.com>
2023-10-03 22:34:11 +0700

    second commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import viewEngine from './configs/viewEngine'
+import initWebRoutes from './route/web'
+require('dotenv').config();
+
+let app = express();
+
+//config app
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+viewEngine(app)
+initWebRoutes(app)
+
+let port = process.env.PORT | 6969
+app.listen(port, ()=>{
+    //call back function
+    console.log('backend nodejs is running in port ',port);
+})
